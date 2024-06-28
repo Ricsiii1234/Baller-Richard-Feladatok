a2b58301a7664d8597d215c8995d4e4a65139706
Ricsiii1234 <ricsiballer@gmail.com>
2024-06-28 11:34:47 +0200

    2024.06.27 Állat2 - Járműpark

---
+++ JarmuPark/JarmuPark/Program.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarmuPark
+{
+    abstract class Jarmu
+    {
+        public string Azonosito { get; private set; }
+        public string Rendszam { get; set; }
+        public int GyartasiEv { get; private set; }
+        public double Fogyasztas { get; set; }
+
+        public double FutottKm { get; private set; }
+        public int AktualisKoltseg { get; private set; }
+        public bool Szabad { get; private set; }
+
+        public static int AktualisEv { get; set; }
+        public static int AlapDij { get; set; }
+        public static double Haszonkulcs { get; set; }
+
+
+        //Konstruktorok
+
+        // Ha ismerjük a fogyasztást
+        public Jarmu(string azonosito, string rendszam, int gyartasiEv, double fogyasztas)
+        {
+            this.Azonosito = azonosito;
+            this.Rendszam = rendszam;
+            this.GyartasiEv = gyartasiEv;
+            this.Fogyasztas = fogyasztas;
+            this.Szabad = true;
+        }
+
+        // Ha nem ismerjük a fogyasztást
+        public Jarmu(string azonosito, string rendszam, int gyartasiEv)
+        {
+            this.Azonosito = azonosito;
+            this.Rendszam = rendszam;
+            this.GyartasiEv = gyartasiEv;
+            this.Szabad = true;
+        }
+
+        // Jármű korának kiszámolása
+
+        public int Kor()
+        {
+            return AktualisEv - GyartasiEv;
+        }
+
+        // Fuvarozás
+
+        public bool Fuvaroz(double ut, int benzinAr)
+        {
+            if (Szabad)
+            {
+                FutottKm += ut;
+                AktualisKoltseg = (int)(benzinAr * ut * Fogyasztas / 100);
+                Szabad = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Alap bérletidíj kiszámolása
+
+        public virtual int BerletiDij()
+        {
+            return (int)(AlapDij + AktualisKoltseg + AktualisKoltseg * Haszonkulcs / 100);
+        }
+
+        // Ha végzett legyen Szabad újra
+
+        public void Vegzett()
+        {
+            Szabad = true;
+        }
+
+        public override string ToString()
+        {
+            return "\nA " + this.GetType().Name.ToLower() + " azonosítója: " + Azonosito + "\nrendszáma: " + Rendszam + "\nkora: " + Kor() + " év" + "\nfogyasztása: " + Fogyasztas + "l/100km" + "\na km-óra állása: " + FutottKm + " km.";
+        }
+    }
+
+        class Busz : Jarmu
+        {
+            // Tulajdonságok
+            public int Ferohely { get; private set; }
+            public static double Szorzo { get; set; }
+
+            // Konstruktorok
+
+            // Ha tudjuk a fogyasztást
+            public Busz(string azonosito, string rendszam, int gyartasiEv, double fogyasztas, int ferohely) : base(azonosito, rendszam, gyartasiEv, fogyasztas)
+            {
+                this.Ferohely = ferohely;
+            }
+
+            // Ha nem tudjuk a fogyasztást
+
+            public Busz(string azonosito, string rendszam, int gyartasiEv, int ferohely) : base(azonosito, rendszam, gyartasiEv)
+            {
+                this.Ferohely = ferohely;
+            }
+
+            // metódusok
+            // Kiszámolja a busz bérletidíját
+
+            public override int BerletiDij()
+            {
+                return (int)(base.BerletiDij() + Ferohely * Szorzo);
+            }
+
+            public override string ToString()
+            {
+                return base.ToString() + "\n\tférőhelyek száma: " + Ferohely;
+            }
+        }
+
+    
+
+    class Teherauto : Jarmu
+    {
+        // tulajdonságok
+
+        // Konstruktorok, fogyasztással és anélkül
+        public double TeherBiras { get; private set; }
+        public static double Szorzo { get; set; }
+
+        public Teherauto(string azonosito, string rendszam, int gyartasiEv, double fogyasztas, double teherBiras) : base (azonosito, rendszam, gyartasiEv, fogyasztas)
+        {
+            this.TeherBiras = teherBiras;
+        }
+
+        public Teherauto(string azonosito, string rendszam, int gyartasiEv, double teherBiras) : base (azonosito, rendszam, gyartasiEv)
+        {
+            this.TeherBiras = teherBiras;
+        }
+
+        // Metódusok
+        // Kiszámolja a teherautó bérletdíját
+
+        public override int BerletiDij()
+        {
+            return (int)(base.BerletiDij() + TeherBiras * Szorzo);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n\tteherbírás: " + TeherBiras + " tonna.";
+        }
+    }
+
+    class Vezerles
+    {
+        private List<Jarmu> jarmuvek = new List<Jarmu>();
+        private string BUSZ = "busz";
+        private string TEHER_AUTO = "teherautó";
+
+        public void Indit()
+        {
+            StatikusBeallitas();
+            AdatBevitel();
+            Kiir("A regisztrált jármüvek: ");
+            Mukodtet();
+            Kiir("\nA müködés utáni állapot: ");
+            AtlagKor();
+            LegtobbKilometer();
+            Rendez();
+        }
+
+        public void StatikusBeallitas()
+        {
+            // Statik adatok beolvasása (be is lehetne olvasni)
+            Jarmu.AktualisEv = 2015;
+            Jarmu.AlapDij = 1000;
+            Jarmu.Haszonkulcs = 10;
+
+            Busz.Szorzo = 15;
+            Teherauto.Szorzo = 8.5;
+        }
+
+        private void AdatBevitel()
+        {
+            string tipus, rendszam, azonosito;
+            int gyartEv, ferohely;
+            double fogyasztas, teherbiras;
+
+            StreamReader olvasocsatorna = new StreamReader("jarmuvek.txt");
+
+            int sorszam = 1;
+
+            while (!olvasocsatorna.EndOfStream)
+            {
+                tipus = olvasocsatorna.ReadLine();
+                Console.WriteLine(tipus);
+                rendszam = olvasocsatorna.ReadLine();
+                gyartEv = int.Parse(olvasocsatorna.ReadLine());
+                fogyasztas = double.Parse(olvasocsatorna.ReadLine());
+                azonosito = tipus.Substring(0, 1).ToUpper() + sorszam;
+
+                if (tipus == BUSZ)
+                {
+                    ferohely = int.Parse(olvasocsatorna.ReadLine());
+                    jarmuvek.Add(new Busz(azonosito, rendszam, gyartEv, fogyasztas, ferohely));
+                }
+                else if (tipus == TEHER_AUTO)
+                {
+                    teherbiras = double.Parse(olvasocsatorna.ReadLine());
+                    jarmuvek.Add(new Teherauto(azonosito, rendszam, gyartEv, fogyasztas, teherbiras));
+                }
+                sorszam++;
+            }
+            olvasocsatorna.Close();
+        }
+
+        private void Kiir(string cim)
+        {
+            Console.WriteLine(cim);
+            foreach(Jarmu jarmu in jarmuvek)
+            {
+                Console.WriteLine(jarmu);
+            }
+        }
+
+        private void Mukodtet()
+        {
+            int osszKoltseg = 0, osszBevetel = 0;
+
+            Random rand = new Random();
+            int alsoBenzinAr = 400, felsoBenzinAr = 420;
+            double utMax = 300;
+            int mukodesHatar = 200;
+            int jarmuIndex;
+
+            Jarmu jarmu;
+            int fuvarSzam = 0;
+
+            for (int i = 0; i < (int)rand.Next(mukodesHatar); i++)
+            {
+                jarmuIndex = rand.Next(jarmuvek.Count());
+                jarmu = jarmuvek[jarmuIndex];
+                if (jarmu.Fuvaroz(rand.NextDouble() * utMax, rand.Next(alsoBenzinAr, felsoBenzinAr)))
+                {
+                    fuvarSzam++;
+                    Console.WriteLine("\nAz induló jármű rendszáma: " + jarmu.Rendszam + "\nAz aktuális fuvarozási költség: " + jarmu.AktualisKoltseg + " Ft." + "\nAz aktuális bevétel: " + jarmu.BerletiDij() + " Ft.");
+
+                    osszBevetel += jarmu.BerletiDij();
+                    osszKoltseg += jarmu.AktualisKoltseg;
+                }
+
+                jarmuIndex = rand.Next(jarmuvek.Count);
+                jarmuvek[jarmuIndex].Vegzett();
+            }
+
+            Console.WriteLine("\n\nA cég teljes költsége: " + osszKoltseg + " Ft." + "\n\nTeljes bevétele: " + osszBevetel + " Ft." + "\n\nHaszna: " + (osszBevetel - osszKoltseg) + " Ft.");
+            Console.WriteLine("\nFuvarok száma: " + fuvarSzam);
+        }
+
+
+        private void AtlagKor()
+        {
+            double osszKor = 0;
+            int darab = 0;
+            foreach (Jarmu jarmu in jarmuvek)
+            {
+                osszKor += jarmu.Kor();
+                darab++;
+            }
+
+            if (darab > 0)
+            {
+                Console.WriteLine("\nA járművek átlag kora: " + osszKor / darab + " év.");
+            }
+            else
+            {
+                Console.WriteLine("Nincsenek járművek.");
+            }
+        }
+
+        private void LegtobbKilometer()
+        {
+            double max = jarmuvek[0].FutottKm;
+            foreach (Jarmu jarmu in jarmuvek)
+            {
+                if (jarmu.FutottKm == max)
+                {
+                    Console.WriteLine(jarmu.Rendszam);
+                }
+            }
+        }
+
+        private void Rendez()
+        {
+            Jarmu temp;
+
+            for (int i = 0; i < jarmuvek.Count() - 1; i++)
+            {
+                for (int j = i; (j < jarmuvek.Count()); j++)
+                {
+                    if (jarmuvek[i].Fogyasztas > jarmuvek[j].Fogyasztas)
+                    {
+                        temp = jarmuvek[j];
+                        jarmuvek[i] = jarmuvek[j];
+                        jarmuvek[j] = temp;
+
+                    }
+                }
+            }
+
+            Console.WriteLine("\nA járművek fogyasztása szerint rendezve: ");
+
+            foreach (Jarmu jarmu in jarmuvek)
+            {
+                Console.WriteLine("{0,10} {1:00.0} liter / 100km.", jarmu.Rendszam, jarmu.Fogyasztas);
+            }
+        }
+
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<Jarmu> jarmuvek = new List<Jarmu>();
+
+                for (int i = 0; i < jarmuvek.Count(); i++)
+            {
+                Console.WriteLine(jarmuvek[i].Rendszam + " bérletdíja " + jarmuvek[i].BerletiDij() + " Ft.");
+            }
+
+            }
+        }
+}
---
+++ Állat2/Állat2/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Állatmenhely
+{
+    class Allat
+    {
+        // mezők
+
+        public string nev { get; private set; }
+        public int szuletesiEv { get; private set; }
+        public int rajtSzam{ get; private set; }
+
+        public int szepsegPont { get; private set; }
+        public int viselkedesPont { get; private set; }
+
+        private static int aktualisEv { get; set; }
+        private static int korHatar { get; set; }
+
+        // konstruktor
+        public Allat(int rajtSzam, string nev, int szuletesiEv)
+        {
+            this.nev = nev;
+            this.szuletesiEv = szuletesiEv;
+            this.rajtSzam = rajtSzam;
+        }
+
+        // metódusok
+        public int Kor()
+        {
+            return aktualisEv - szuletesiEv;
+        }
+
+        public virtual int PontSzam()
+        {
+            if (Kor() < korHatar)
+            {
+                return viselkedesPont * Kor() + szepsegPont * (korHatar - Kor());
+            }
+            return 0;
+        }
+
+
+        public void Pontozzak(int szepsegPont, int viselkedesPont)
+        {
+            this.szepsegPont = szepsegPont;
+            this.viselkedesPont = viselkedesPont;
+        }
+
+        public override string ToString()
+        {
+            return rajtSzam + ". " + nev + " nevű" + this.GetType().Name.ToLower() + " pontszáma: " + PontSzam() + " pont.";
+        }
+
+        // Leszármaztatás
+
+        class Kutya : Allat
+        {
+            public int GazdaViszonyPont { get; private set; }
+            public bool KapottViszonyPont { get; private set; }
+
+            public Kutya(int rajtSzam, string nev, int szuletesiEv) : base (rajtSzam, nev, szuletesiEv)
+            {
+
+            }
+
+            public void ViszonPontozas(int gazdaViszonyPont)
+            {
+                this.GazdaViszonyPont = gazdaViszonyPont;
+                KapottViszonyPont = true;
+            }
+
+            public override int PontSzam()
+            {
+                int pont = 0;
+                if (KapottViszonyPont)
+                {
+                    pont = base.PontSzam() + GazdaViszonyPont;
+                }
+                return pont;
+            }
+        }
+
+        class Macska : Allat
+        {
+            public bool VanMacskaSzallitoDoboz { get; set; }
+            public Macska(int rajtSzam, string nev, int szuletesiEv, bool vanMacskaSzallitoDoboz) : base (rajtSzam, nev, szuletesiEv)
+            {
+                this.VanMacskaSzallitoDoboz = vanMacskaSzallitoDoboz;
+            }
+
+            public override int PontSzam()
+            {
+                if (VanMacskaSzallitoDoboz)
+                {
+                    return base.PontSzam();
+                }
+                return 0;
+            }
+        }
+
+        class Vezerles
+        {
+            private List<Allat> allatok = new List<Allat>();
+
+            public void Start()
+            {
+                Allat.AktualisEv = 2015;
+                Allat.KorHatar = 10;
+            }
+
+
+
+
+            private void Proba()
+            {
+                Allat allat1, allat2;
+
+                string nev1 = "Pamacs", nev2 = "Bolhazsák";
+                int szuletesiEv1 = 2010, szuletesiEv2 = 2011;
+                bool vanDoboz = true;
+                int rajtSzam = 1;
+
+                int szepsegPont = 5, viselkedePont = 4, viszonyPont = 6;
+
+                allat1 = new Kutya(rajtSzam, nev1, szuletesiEv1);
+                rajtSzam++;
+
+                allat2 = new Macska(rajtSzam, nev2, szuletesiEv2, vanDoboz);
+
+                Console.WriteLine("A regisztrált állatok: ");
+                Console.WriteLine(allat1);
+                Console.WriteLine(allat2);
+
+                // verseny:
+                allat2.Pontozzak(szepsegPont, viselkedePont);
+                
+                if (allat1 is Kutya)
+                {
+                    (allat1 as Kutya).ViszonPontozas(viszonyPont);
+                }
+                
+                allat1.Pontozzak(szepsegPont, viselkedePont);
+            }
+
+            private void Regisztracio()
+            {
+                StreamReader olvasoCsatorna = new StreamReader("allatok.txt");
+
+                string fajta, nev;
+                int rajtSzam = 1, szuletesiEv;
+                bool vanDoboz;
+
+                // Olvasás a fájl végéig
+
+                while (!olvasoCsatorna.EndOfStream)
+                {
+                    fajta = olvasoCsatorna.ReadLine();
+                    nev = olvasoCsatorna.ReadLine();
+                    szuletesiEv = int.Parse(olvasoCsatorna.ReadLine());
+
+                    // Példányok létrehozása
+
+                    if (fajta == "kutya")
+                    {
+                        allatok.Add(new Kutya(rajtSzam, nev, szuletesiEv));
+                    }
+                    else
+                    {
+                        vanDoboz = bool.Parse(olvasoCsatorna.ReadLine());
+                        allatok.Add(new Macska(rajtSzam, nev, szuletesiEv, vanDoboz));
+                    }
+                    rajtSzam++;
+                }
+                olvasoCsatorna.Close();
+            }
+
+            private void Verseny()
+            {
+                Random rand = new Random();
+                int hatar = 11;
+                foreach (Allat item in allatok)
+                {
+                    if (item is Kutya)
+                    {
+                        (item as Kutya).ViszonPontozas(rand.Next(hatar));
+                    }
+                    item.Pontozzak(rand.Next(hatar) , rand.Next(hatar));
+                }
+            }
+
+        }
+        public int RajtSzam
+        {
+            get { return rajtSzam; }
+            set { rajtSzam = value; }
+        }
+
+        // kívültről változtatható értékek
+
+        public static int AktualisEv
+        {
+            get { return aktualisEv; }
+            set { aktualisEv = value; }
+        }
+        public static int KorHatar
+        {
+            get { return korHatar; }
+            set { korHatar = value; }
+        }
+
+        
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Vezerles().Start();
+
+            Console.ReadKey();
+        }      
+    }
+}
